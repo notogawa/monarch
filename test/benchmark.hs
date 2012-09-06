3836bad5fe100eed68ed065b9618e7ec30f19272
notogawa <n.ohkawa@gmail.com>
2012-09-06 22:49:39 +0900

    Add benchmark test. (v0.2.0.0 - v0.4.0.0 are slow ?)

---
+++ test/benchmark.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Database.Monarch
+
+import qualified Database.TokyoTyrant.FFI as FFI
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.ByteString.Char8 ()
+
+main :: IO ()
+main = monarch
+
+ffi :: IO ()
+ffi = do
+  Right conn <- FFI.open "localhost" 1978
+  forM_ [1..1000::Int] $ \_ -> do
+    FFI.put conn "foo" "bar"
+    FFI.get conn "foo"
+    return ()
+  FFI.close conn
+
+monarch :: IO ()
+monarch = do
+  withMonarchConn "localhost" 1978 $ runMonarchConn $ do
+         forM_ [1..1000::Int] $ \n -> do
+            liftIO $ print n
+            put "foo" "bar"
+            get "foo"
+            return ()
+  return ()
