f480189bb853c1bcc7898b85fcfe34cbfb19b0eb
notogawa <n.ohkawa@gmail.com>
2013-09-21 23:27:22 +0900

    Relax dependencies.

--- test/doctests.hs
+++ test/doctests.hs
@@ -1,6 +1,4 @@
 import Test.DocTest
 
 main :: IO ()
-main = doctest [ "-Lcabal-dev/lib"
-               , "-package-conf=cabal-dev/packages-7.4.2.conf"
-               , "Database/Monarch/Utils.hs" ]
+main = doctest [ "Database.Monarch.Utils" ]
